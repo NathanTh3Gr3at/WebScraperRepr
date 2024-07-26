32498704017d444666472995a111a159cc5680d9
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2024-07-26 16:26:28 +0200

    added scraper

---
+++ Scraper.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://quotes.toscrape.com"
+request=requests.get(url)
+soup=BeautifulSoup(request.content,'html.parser')
+content=soup.find('div', class_='quote')
+lines=content.find('span',class_='text')
+author=content.find('small',class_='author')
+output=f"{lines} by {author}"
+
+print(output)
